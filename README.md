788c3f3b6880837c1a8a2e6f4b51d08fa6bb33ce
Tristan Kirksey <kirkseytc@gmail.com>
2025-04-15 20:21:28 -0400

    words list finished

---
+++ README.md
@@ -0,0 +1,2 @@
+# Wordle by the C
+Wordle by the C is a beach themed version of wordle written in C.
---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+const char wordpool[12][6] = {
+    "beach",
+    "coral",
+    "ocean",
+    "shore",
+    "islet",
+    "sunny",
+    "salty",
+    "drift",
+    "guava",
+    "mango",
+    "oasis",
+    "aloha"
+};
+
+int main(void){
+
+    srand(time(NULL));
+
+    return 0;
+
+}
