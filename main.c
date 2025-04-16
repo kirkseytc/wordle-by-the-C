03201d30284cf97404ccef5659b43e3eb756ee6c
Tristan Kirksey <kirkseytc@gmail.com>
2025-04-15 20:34:04 -0400

    added stdin flushing

--- main.c
+++ main.c
@@ -17,9 +17,18 @@ const char wordpool[12][6] = {
     "aloha"
 };
 
+void flush_stdin(){
+    int c;
+    while((c = getchar()) != '\n' && c != EOF);
+}
+
 int main(void){
 
     srand(time(NULL));
+
+    char buffer[6];
+    
+    flush_stdin();
 
     return 0;
 
